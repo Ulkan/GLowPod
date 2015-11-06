504e6bb79c5e8661f5b29add9cfc17ed4bc22b72
BeardMage <mesk.utp@gmail.com>
2015-11-06 17:12:54 +0100

    New Controller
    Added new controller, lock option etc.

---
+++ Assets/NewRotor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewRotor : MonoBehaviour 
+{
+	public float rotateSpeed;
+	bool isLocked;
+	float actualSpeed;
+	float dirRotate;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		actualSpeed = rotateSpeed;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+
+
+		if (isLocked)
+		{
+			transform.Rotate( new Vector3 ( actualSpeed * Time.deltaTime * dirRotate,0,0));
+		}
+
+		if(Input.GetAxis("Horizontal") != 0f)
+		{
+			transform.Rotate ( new Vector3 ( 0,actualSpeed * Time.deltaTime * Input.GetAxis("Horizontal") * -1f ,0));
+		}
+
+		if(Input.GetAxis("Vertical") != 0f && !isLocked)
+		{
+			dirRotate = Input.GetAxis ("Vertical");
+			transform.Rotate ( new Vector3 ( actualSpeed * Time.deltaTime * Input.GetAxis("Vertical"),0,0));
+		}
+
+		if ( Input.GetButtonDown("Lock"))
+		{
+			if (isLocked) isLocked = false;
+			else isLocked = true;
+			
+		}
+	}
+}
